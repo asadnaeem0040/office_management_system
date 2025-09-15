ea4abbd894d6daaf9a7dec5a2e81938cdbed6dab
Asad Naeem <asadnaeem1717@gmail.com>
2025-09-15 18:33:49 +0500

    Pojos/models made

---
+++ src/main/java/models/Employee.java
@@ -0,0 +1,28 @@
+package models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Entity
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String email;
+    private double salary;
+    private Date joiningDate;
+
+    @ManyToOne
+    @JoinColumn(name = "department_id")
+    private Department department;
+}
